84b1621bef79fb4a87f214cc3ad46d884ed26ccf
Everson Barbosa <eversonlpb@gmail.com>
2022-02-04 02:19:10 -0300

    Correção Exercícios

---
+++ curso-java-básico/src/com/loiane/cursojava/aula12/CorrecaoExercicios.java
@@ -0,0 +1,158 @@
+package com.loiane.cursojava.aula12;
+
+import java.util.Scanner;
+
+public class CorrecaoExercicios {
+	public static void main(String[] args) {
+		
+		/* //1.
+		
+		System.out.println("Hello World");
+		
+		// 2.  
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Informe um número inteiro:");
+		
+		int numero = scan.nextInt();
+		
+		System.out.println("O número informado foi " + numero); 
+		
+		// 3. 
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Informe o primero número");
+		int primeiroNumero = scan.nextInt();
+		System.out.println("Informe o segundo número");
+		int segundoNumero = scan.nextInt();
+		int resultado = primeiroNumero + segundoNumero;
+		System.out.println("O resultado da soma dos numeros informados é: " + resultado); 
+		
+		// 4. 
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Informe as quatro notas bimestrais (separadas por espaço):");
+		double primeiraNota = scan.nextDouble();
+		double segundaNota = scan.nextDouble();
+		double terceiraNota = scan.nextDouble();
+		double quartaNota = scan.nextDouble();
+		double resultado = (primeiraNota + segundaNota + terceiraNota + quartaNota) / 4;
+		System.out.println("A média das notas informadas é: " + resultado); 
+		
+		// 5.
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Informe o valor em metros a ser convertido para centímetros:");
+		double metros = scan.nextDouble();
+		double resultado = metros * 100;
+		System.out.println("O resultado é: " + resultado+ "cm."); 
+		
+		// 6. 
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Informe o raio do círculo cuja área será calculada:");
+		double raio = scan.nextDouble();
+		double area = Math.PI * Math.pow(raio, 2);
+		System.out.println("A área aproximada do círculo é: " + area); 
+		
+		// 7.
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Informe o tamanho do lado do quadrado:");
+		double lado = scan.nextDouble();
+		double area = Math.pow(lado, 2);
+		
+		System.out.println("O dobro da área do quadrado é " + area); 
+		
+		// 8. 
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Informe o valor da hora trabalhada:");
+		double salarioPorHora = scan.nextDouble();
+		System.out.println("Informe o número de horas trabalhadas no mês:");
+		double horasTrabalhadas = scan.nextDouble();
+		double resultado = horasTrabalhadas * salarioPorHora;
+		System.out.println("O valor a ser recebido é: " + resultado);
+		
+		// 9.
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Informe a temperatura em Fahrenheit:");
+		double temperatura = scan.nextDouble();
+		double resultado = (5* (temperatura - 32) / 9 );
+		System.out.println("A temperatura em Celsius é: " + resultado); 
+		
+		// 10.
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Informe a temperatura em Celsius:");
+		double temperatura = scan.nextDouble();
+		double resultado = (temperatura * 1.8 ) + 32;
+		System.out.println("A temperatura em Fahrenheit é: " + resultado); 
+		
+		// 11. 
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Entre com o primeiro númerio inteiro:");
+		int num1 = scan.nextInt();
+		System.out.println("Entre com o segundo número inteiro:");
+		int num2 = scan.nextInt();
+		System.out.println("Entre com um número real:");
+		double num3 = scan.nextDouble();
+		
+		int resultado1 = (num1*2) * (num2/2);
+		double resultado2 = (num1*3) + num3;
+		double resultado3 = Math.pow(num3, 3);
+
+		System.out.println("Resultado A: " + resultado1);
+		System.out.println("Resultado B: " + resultado2);
+		System.out.println("Resultado C: " + resultado3); 
+		
+		// 12.
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Informe a altura:");
+		double altura = scan.nextDouble();
+		double pesoIdeal = (72.7 * altura) - 58;
+		
+		System.out.println("O peso ideial é: " + pesoIdeal); 
+		
+		// 13. 
+		
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Informe o valor da hora trabalhada:");
+		double salarioPorHora = scan.nextDouble();
+		System.out.println("Informe o número de horas trabalhadas no mês:");
+		double horasTrabalhadas = scan.nextDouble();
+		double salarioBruto = horasTrabalhadas * salarioPorHora;
+		double inss = (salarioBruto / 100) * 8;
+		double sindicato = (salarioBruto/ 100) * 5;
+		double ir = (salarioBruto/ 100) * 11;
+		double totalDeDescontos = inss + sindicato + ir; 	
+		double salarioLiquido = salarioBruto - totalDeDescontos;
+		System.out.println("O salário bruto é: " + salarioBruto);
+		System.out.println("INSS " + inss);
+		System.out.println("Sindicato " + sindicato);
+		System.out.println("IR "+ ir);
+		System.out.println("Total de descontos " + totalDeDescontos);
+		System.out.println("Salário líquido " + salarioLiquido); 
+		
+		// 14. 
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Entre com o tamanho do arquivo:");
+		double arquivo = scan.nextDouble();
+		System.out.println("Entre com a velocidade da internet:");
+		double velocidade = scan.nextDouble();
+		double tempo = arquivo / velocidade; */
+		
+		
+		
+	}
+}
